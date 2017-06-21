5ab6379b5332626036b66300be857b47c4846c49
Cait Rocco <cait.rocco@gmail.com>
2017-06-21 10:29:33 -0500

    display JSON messages in the html

--- premessage.js
+++ premessage.js
@@ -3,6 +3,8 @@
 
 	let messages = null;
 
+	
+
 	function getMessages () {
 		let requestMess = new XMLHttpRequest();
 		requestMess.addEventListener("load", setMessages);
@@ -11,9 +13,19 @@
 	}
 
 	function setMessages () {
-		messages = JSON.parse(event.target.responseText).messages;
+		messages = JSON.parse(event.target.responseText).preloaded_messages;
+		displayMessages(messages)
+	}
+
+	function displayMessages (message) {
+		// console.log("message", messages);
+		messages.forEach(function(message) {
+			let messOutput = document.getElementById('msg-output');
+			messOutput.innerHTML += `${message.message}<br>`;
+			})
 	}
 
 	window.Chatty = window.Chatty || {};
-	Chatt.LoadMessages = loadMessages;
+	Chatty.LoadMessages = loadMessages;
 }
+getMessages();
