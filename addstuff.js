a31957b5eaca523993b2be72ff7f21cbb450fcc2
Rian501 <eclaire.lemmon@gmail.com>
2017-06-21 09:47:15 -0500

    clean up and link addstuff.js

--- addstuff.js
+++ addstuff.js
@@ -1,15 +1,18 @@
+console.log("well at least it's linked", );
+
 var Chatty = (function (prevChatty) {
-	let messageArray = //initially, our 5 JSON messages, as an imported private array
-	
+	let messageArray = [] //initially, our 5 JSON messages, as an imported private array
+
 
 	let inputBox = document.getElementById('msg-input');
-	inputBox.addEventListener('keyup', function(event) {
+	inputBox.addEventListener('keyup', function() {
 		if (event.key === 'Enter')  {
+			console.log("input", inputBox.value);
 			userNewMsg = inputBox.value;
 			messageArray.push(userNewMsg);
 			//TODO call the function that gets the input and converts it to a part of the private array
 		}
-	}
+	});
 
 
 
