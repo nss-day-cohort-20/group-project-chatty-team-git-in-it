dcd51a50f739eb9c67d711c9ee295d8d1a27b154
Rian501 <eclaire.lemmon@gmail.com>
2017-06-21 11:14:39 -0500

    Add preloaded messages to the array

--- premessage.js
+++ premessage.js
@@ -12,18 +12,21 @@
 
 	function setMessages () {
 		messages = JSON.parse(event.target.responseText).preloaded_messages;
-		displayMessages(messages)
+		messageArray(messages);
 	}
 
-	function displayMessages (message) {
+	let messageArray = function arrayUpMessages (message) {
 		// console.log("message", messages);
+			let	msgArray = []
 		messages.forEach(function(message) {
-			let messOutput = document.getElementById('msg-output');
-			messOutput.innerHTML += `${message.message}<br>`;
+			// let messOutput = document.getElementById('msg-output');
+			// messOutput.innerHTML += `${message.message}<br>`;
+			msgArray.push(message.message);
 			})
+			console.log("message array?", msgArray);
+		return msgArray
 	}
 
 	window.Chatty = window.Chatty || {};
 	Chatty.LoadMessages = loadMessages;
-	getMessages();
 }
