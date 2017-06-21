5319812981061a067b7867775aac431c56dab3f9
Rian501 <eclaire.lemmon@gmail.com>
2017-06-21 09:38:33 -0500

    Add message to array

--- addstuff.js
+++ addstuff.js
@@ -1,11 +1,16 @@
 var Chatty = (function (prevChatty) {
+	let messageArray = //initially, our 5 JSON messages, as an imported private array
+	
+
 	let inputBox = document.getElementById('msg-input');
 	inputBox.addEventListener('keyup', function(event) {
 		if (event.key === 'Enter')  {
+			userNewMsg = inputBox.value;
+			messageArray.push(userNewMsg);
 			//TODO call the function that gets the input and converts it to a part of the private array
 		}
 	}
 
-	
+
 
 })(Chatty || {});
