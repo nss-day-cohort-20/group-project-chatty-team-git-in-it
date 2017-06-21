aadc26d9cf8b8237dcfb59d583e6ae4faea48282
Hayley Landsberg <hayleyheath@Hayleys-MacBook-Air.local>
2017-06-21 15:57:58 -0500

    made dark and large themes functional

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ function outputMessages (messagesArray)  {
 }
 
 Chatty.LoadMessages.retrieveMessages(outputMessages);
+
+
+let addTheme = document.querySelector(".theme");
+
+document.getElementById("dark-theme-box").addEventListener("click", function() {
+	addTheme.classList.toggle("dark");
+});
+
+document.getElementById("large-text-box").addEventListener("click", function() {
+	addTheme.classList.toggle("large");
+});
