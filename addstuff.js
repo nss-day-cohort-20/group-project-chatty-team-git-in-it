d93cb636cc1f43a59303af5ccdc59a6106992aef
An-AngryBear <monahajt@gmail.com>
2017-06-21 11:00:49 -0500

    added a function to addstuff.js that puts the last entry into the message array, into the dom

--- addstuff.js
+++ addstuff.js
@@ -1,19 +1,26 @@
 console.log("well at least it's linked", );
 
-var Chatty = (function (prevChatty) {
+{
+	let addMessages = Object.create(null);
 	let messageArray = [] //initially, our 5 JSON messages, as an imported private array
-
-
+	let messageOutputDiv = document.getElementById("msg-output");
 	let inputBox = document.getElementById('msg-input');
+
 	inputBox.addEventListener('keyup', function() {
 		if (event.key === 'Enter')  {
 			console.log("input", inputBox.value);
-			userNewMsg = inputBox.value;
+			let userNewMsg = inputBox.value;
 			messageArray.push(userNewMsg);
 			//TODO call the function that gets the input and converts it to a part of the private array
-		}
+			putUserMsgInDom();
+		};
 	});
 
+	function putUserMsgInDom() {
+			let messageBlock = document.createElement('div');
+			messageBlock.innerHTML = `<p class="msg">${messageArray[messageArray.length -1]}</p><button class="delete-btn">Delete</button>`;
+			messageOutputDiv.appendChild(messageBlock);
+	}
 
-
-})(Chatty || {});
+	window.Chatty = window.Chatty || {};
+}
