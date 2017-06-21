6f076fe51b96d0dc8c1f2bf7ccab71c3f40b9b6f
Rian501 <eclaire.lemmon@gmail.com>
2017-06-21 09:26:23 -0500

    listen for enter keypress

---
+++ addstuff.js
@@ -0,0 +1,9 @@
+var Chatty = (function (prevChatty) {
+	let inputBox = document.getElementById('msg-input');
+	inputBox.addEventListener('keyup', function(event) {
+		if (event.key === 'Enter')  {
+			//call the function that gets the input and converts it to a part of the private array
+		}
+	}
+
+})(Chatty || {});
