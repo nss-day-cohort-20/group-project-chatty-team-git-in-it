46992850300c58aee172136f5dd0516762669ec3
Rian501 <eclaire.lemmon@gmail.com>
2017-06-21 09:30:52 -0500

    move readmes to readme folder

--- addstuff.js
+++ addstuff.js
@@ -2,8 +2,10 @@ var Chatty = (function (prevChatty) {
 	let inputBox = document.getElementById('msg-input');
 	inputBox.addEventListener('keyup', function(event) {
 		if (event.key === 'Enter')  {
-			//call the function that gets the input and converts it to a part of the private array
+			//TODO call the function that gets the input and converts it to a part of the private array
 		}
 	}
+
+	
 
 })(Chatty || {});
