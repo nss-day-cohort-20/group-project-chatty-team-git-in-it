561dd55605f6706b3f47cdb8391774888e976884
Cait Rocco <cait.rocco@gmail.com>
2017-06-23 13:42:46 -0500

    fixed order of messages appearing on screen

--- DOMInteraction.js
+++ DOMInteraction.js
@@ -9,9 +9,6 @@
 		messageArray.unshift(userNewMsg);
 	};
   
-// One IIFE should contain a function that accepts an element id, and the user message,
-// and then add the user's message - along with the delete button - to the specified parent element.
-// Each message should be stored in a private array in this IIFE. This IIFE should also expose a function to read all messages, and delete a single message.
   addMessages.putMsgsInDom = function (id, msg) {
 		let messageBlock = document.createElement('div');
 		messageBlock.setAttribute("id", `${id}`)
@@ -24,7 +21,6 @@
 			addMessages.putMsgsInDom(i, arrayToBeAssignedIds[i])
 		}
 	}
-
 
 	addMessages.putJsonMsgInDom = function (messageArray)  {
 		let messageBlock = document.createElement('div');
@@ -42,6 +38,7 @@
 		return Arr1.concat(Arr2);
 	}
 
+	//functionality to change to dark theme or enlarged text
 	let addTheme = document.querySelector(".theme");
 
 	document.getElementById("dark-theme-box").addEventListener("click", function() {
