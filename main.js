d922443aa34479b4b1d713f10e8363a6c031079e
Hayley Landsberg <hayleyheath@Hayleys-MacBook-Air.local>
2017-06-23 15:27:39 -0500

    worked on trying to get the delete button to work without adding out array items in

--- main.js
+++ main.js
@@ -4,11 +4,16 @@ let inputBox = document.getElementById('msg-input');
 let jsonArr = Chatty.LoadMessages.getMessages();
 let userArr = Chatty.DOMInteraction.getUserMessages();
 
+// window.addEventListener("load", function() {
+
+// });
+
+
 inputBox.addEventListener('keyup', function() {
 		if (event.key === 'Enter') {
 			Chatty.DeleteMess.clearElement();
 			Chatty.DOMInteraction.addToArray();
-			combinedArray = Chatty.DOMInteraction.combineAllMessages(jsonArr, userArr)
+			let combinedArray = Chatty.DOMInteraction.combineAllMessages(jsonArr, userArr)
 			// let bigArray = Chatty.DOMInteraction.combineAllMessages(jsonArr, userArr);
 			Chatty.DOMInteraction.assignIds(combinedArray);//(bigArray);
 			// Chatty.DOMInteraction.putUserMsgInDom("id", inputBox.value);
@@ -24,3 +29,12 @@ clear.addEventListener("click", function() {
 		Chatty.DeleteMess.disableBtn();
 		console.log("clear array?", combinedArray);
 	});
+
+window.addEventListener("click", function() {
+	if (event.target.classList.contains("delete-btn")) {
+		let parentID = event.target.parentNode.getAttribute("id");
+		combinedArray = Chatty.DOMInteraction.combineAllMessages(jsonArr, userArr);
+		Chatty.DeleteMess.removeMessage(parentID);
+		combinedArray = Chatty.DeleteMess.removeFromArray(combinedArray, parentID);
+	}
+});
